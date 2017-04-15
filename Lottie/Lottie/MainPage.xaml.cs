b9f48e88ec1405835e7a9baa7affc45d0f532d82
JinJia Hsu <JinJia Hsu>
2017-04-15 17:33:26 +0800

    first commit

---
+++ Lottie/Lottie/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Lottie
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            animationView.Progress = (float)e.NewValue;
+        }
+    }
+}
